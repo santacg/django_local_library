3503e1a36ad44f3087c136d556d4a2845cbc9f63
santacg <garciasantacarlos@gmail.com>
2024-02-21 00:05:32 +0100

    populate y superuser

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
-python3 manage.py collectstatic --no-input
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 populate_catalog.py
-python3 manage.py createsu
+python manage.py collectstatic --no-input
+python manage.py makemigrations
+python manage.py migrate
+python populate_catalog.py
+python manage.py createsuperuser
