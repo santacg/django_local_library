5e96b100ce977eb19f1fe23a5f54e38448a89915
santacg <garciasantacarlos@gmail.com>
2024-02-20 22:06:44 +0100

    correcion static path

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -136,7 +136,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'  #. os.path.join(BASE_DIR, 'staticfiles')
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
