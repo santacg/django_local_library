1cc1adc7d04a9107ce2b9efa36cba2414e0e9ccd
santacg <garciasantacarlos@gmail.com>
2024-02-20 22:36:45 +0100

    url database

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -146,3 +146,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
