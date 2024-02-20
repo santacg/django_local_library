87f1961c907f4b188a62df3e7ef07989ddf5ef5d
santacg <garciasantacarlos@gmail.com>
2024-02-21 00:22:53 +0100

    quito populate

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-python populate_catalog.py
+# python populate_catalog.py
 python manage.py createsuperuser
